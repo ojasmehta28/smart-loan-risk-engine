c42dc97d402bfaae70e77a1eb5335202e72ef2f4
Ojas Mehta <63046043+ojasmehta28@users.noreply.github.com>
2026-04-19 05:26:46 +0530

    Added getters and setters to LoanApplication entity to enable field access in service layer

--- src/main/java/com/loan/riskengine/entity/LoanApplication.java
+++ src/main/java/com/loan/riskengine/entity/LoanApplication.java
@@ -18,4 +18,30 @@ public class LoanApplication {
     private double loanAmount;
 
     private String status;
+
+    public double getIncome() {
+        return income;
+    }
+    public void setIncome(double income) {
+        this.income = income;
+    }
+    public int getCreditScore() {
+        return creditScore;
+    }
+    public void setCreditScore(int creditScore){
+        this.creditScore = creditScore;
+    }
+    public double getLoadAmount(){
+        return loanAmount;
+    }
+    public void setLoanAmount(double loanAmount){
+        this.loanAmount = loanAmount;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
 }
